87a9534480da4739e8564fcc42a08f5af70f1e18
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-05-06 12:12:15 +0200

    feat: add searchbar

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,10 +1,28 @@
+
 import React from 'react'
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 
 const GlobalContext = createContext()
 
-const GlobalProvider = ({children}) => {
-const value = {}
+const GlobalProvider = ({ children }) => {
+
+  const [cars, setCars] = useState([])
+  const [filteredCars, setFilteredCars] = useState([]);
+
+  const fetchCars = async () => {
+    const response = await fetch(`http://localhost:3001/cars`)
+    const data = await response.json()
+    setCars(data)
+    setFilteredCars(data);
+  }
+
+  useEffect(() => {
+    fetchCars()
+  }, [])
+
+
+
+  const value = { fetchCars, cars, filteredCars, setFilteredCars }
   return (
     <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>
   )
@@ -14,4 +32,4 @@ const useGlobalContext = () => {
   return useContext(GlobalContext)
 }
 
-export {GlobalProvider, useGlobalContext}
+export { GlobalProvider, useGlobalContext }
--- src/pages/CarsList.jsx
+++ src/pages/CarsList.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { useGlobalContext } from '../context/GlobalContext'
 
 const CarsList = () => {
+
+  const { cars, filteredCars, setFilteredCars } = useGlobalContext()
+
+
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    const filtered = cars.filter(car => car.title.toLowerCase().includes(searchTerm));
+    setFilteredCars(filtered);
+  };
+
+
+
   return (
-    <div>CarsList</div>
+    <div className='container my-5'>
+      <label className='mb-3 w-100'>
+        <div class="input-group mb-3">
+          <input type="text" className="form-control w-100" placeholder="Cerca un'automobile..."
+            onChange={handleSearch} />
+        </div>
+      </label>
+      {filteredCars && filteredCars.length > 0 ? (
+        filteredCars.map((car, index) => <p key={index} className='car text-center'>{car.title}</p>)
+      ) : (
+        <p>Nessuna macchina disponibile</p>
+      )}
+
+
+    </div>
   )
 }
 
